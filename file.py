c9fc576ad983882e949210c7873b859483f1cf6f
Jeremiah S. Visbal || JSV <156052449+Kiyasekage@users.noreply.github.com>
2026-07-30 06:04:42 +0800

    Create file.py

---
+++ file.py
@@ -0,0 +1,8 @@
+import random
+print("Welcome to Find The Gold!")
+name = input("What's your name? ")
+maps = [["■","■","■"],["■","■","■"],["■","■","■"]]
+print(maps[0])
+print(maps[1])
+print(maps[2])
+print(f"Thankyou for using our program,{name}!")
