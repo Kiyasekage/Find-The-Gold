df401dd5a3f1de6310e25b85b8d92a9ff9c49ba7
Jeremiah S. Visbal || JSV <156052449+Kiyasekage@users.noreply.github.com>
2026-07-30 07:18:17 +0800

    Add intro, rules, row and column index for gold's location

--- file.py
+++ file.py
@@ -1,8 +1,10 @@
 import random
 print("Welcome to Find The Gold!")
+print("Rules : \n1. You will guess the location of the gold\n2. You will guess by entering number of row and column(example : 12, row 1, column 2)\n3. There are three level of achievements, Perfect(able to guess below 3 guesses), average(able to guess below 7 guesses), and Noob(only able to guess above 8 guesses)")
 name = input("What's your name? ")
 maps = [["■","■","■"],["■","■","■"],["■","■","■"]]
-print(maps[0])
-print(maps[1])
-print(maps[2])
+row = random.randint(0,2)
+column = random.randint(0,2)
+gold = maps[row][column]
+print(maps)
 print(f"Thankyou for using our program,{name}!")
