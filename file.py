023fb53496169b16f69cac68af2cc0aad9edd7a5
Jeremiah S. Visbal || JSV <156052449+Kiyasekage@users.noreply.github.com>
2026-08-06 02:25:07 +0800

    moving the if else chance checker to a function and make the map output cleaner

--- file.py
+++ file.py
@@ -1,19 +1,24 @@
 import random
+def checker_chance(p_chance):
+    if p_chance<=3:
+        print("You've achieve perfect level!")
+    elif p_chance>3 and p_chance<=7:
+        print("You've achieve average level!")
+    else:
+        print("You've achieve noob level!")
 print("Welcome to Find The Gold!")
 print("Rules : \n1. You will guess the location of the gold\n2. You will guess by entering number of row and column(example : 12, row 1, column 2)\n3. Row and column starts from 1\n4. There are three level of achievements, Perfect(able to guess below 3 guesses), average(able to guess below 7 guesses), and Noob(only able to guess above 8 guesses)")
 name = input("What's your name? ")
 maps = [["■","■","■"],["■","■","■"],["■","■","■"]]
 row = random.randint(0,2)
 column = random.randint(0,2)
-print(maps[0],maps[1],maps[2])
-print(row,column)
+print("",maps[0],"\n",maps[1],"\n",maps[2])
 chance = 0
 while True:
     ans = int(input("Please guess the gold : "))
     ans = str(ans)
     loc1 = int(ans[0])-1
     loc2 = int(ans[1])-1
-    loc = maps[loc1][loc2]
     print(loc1,loc2)
     if maps[loc1][loc2]=="X":
         print("Cannot enter the same number of row and column as before")
@@ -24,11 +29,6 @@ while True:
     else:
         print("You guessed it wrong!")
         maps[loc1][loc2] = "X"
-        print(maps)
-if chance<=3:
-    print("You've achieve perfect level")
-elif chance>3 and chance<=7:
-    print("You've achieve average level")
-else:
-    print("You've achieve noob level")
+        print("",maps[0],"\n",maps[1],"\n",maps[2])
+checker_chance(chance)
 print(f"Thankyou for using our program,{name}!")
