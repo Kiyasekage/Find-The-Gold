14c7c9a3cb452b559edb0790ae47c90b0a967614
Jeremiah S. Visbal || JSV <156052449+Kiyasekage@users.noreply.github.com>
2026-08-06 01:18:53 +0800

    Adding if else to print out user's output level and adding checker if user enters the same number

--- file.py
+++ file.py
@@ -1,24 +1,34 @@
 import random
 print("Welcome to Find The Gold!")
-print("Rules : \n1. You will guess the location of the gold\n2. You will guess by entering number of row and column(example : 12, row 1, column 2)\n3. There are three level of achievements, Perfect(able to guess below 3 guesses), average(able to guess below 7 guesses), and Noob(only able to guess above 8 guesses)")
+print("Rules : \n1. You will guess the location of the gold\n2. You will guess by entering number of row and column(example : 12, row 1, column 2)\n3. Row and column starts from 1\n4. There are three level of achievements, Perfect(able to guess below 3 guesses), average(able to guess below 7 guesses), and Noob(only able to guess above 8 guesses)")
 name = input("What's your name? ")
 maps = [["■","■","■"],["■","■","■"],["■","■","■"]]
 row = random.randint(0,2)
 column = random.randint(0,2)
-print(row,column)
 print(maps[0],maps[1],maps[2])
+print(row,column)
+chance = 0
 while True:
     ans = int(input("Please guess the gold : "))
     ans = str(ans)
-    loc1 = int(ans[0])
-    loc2 = int(ans[1])
+    loc1 = int(ans[0])-1
+    loc2 = int(ans[1])-1
     loc = maps[loc1][loc2]
     print(loc1,loc2)
+    if maps[loc1][loc2]=="X":
+        print("Cannot enter the same number of row and column as before")
+    chance+=1
     if loc1==row and loc2==column:
-        print("Congratulations, {name}.. You guessed it correctly")
+        print(f"Congratulations, {name}.. You guessed it correctly")
         break
     else:
         print("You guessed it wrong!")
-        maps[loc1][loc2] = "x"
+        maps[loc1][loc2] = "X"
         print(maps)
+if chance<=3:
+    print("You've achieve perfect level")
+elif chance>3 and chance<=7:
+    print("You've achieve average level")
+else:
+    print("You've achieve noob level")
 print(f"Thankyou for using our program,{name}!")
